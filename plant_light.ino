56930a3dc90aafefefb90b6a41e79f363dec0521
Ben Wittbrodt <benwittbrodt@gmail.com>
2020-12-12 10:16:14 -0500

    changed variable names and added pin definition

--- plant_light.ino
+++ plant_light.ino
@@ -1,21 +1,24 @@
 
-/*
-14CORE LCD 16x2 Display Driving Example
-*/
-#include <Wire.h> // Include Wire.h to control I2C
-#include <LiquidCrystal_I2C.h> //Download & include the code library can be downloaded below
-#include <SPI.h>
-#include <SD.h>
+#include <Wire.h> // Include Wire.h to control I2C display
+#include <LiquidCrystal_I2C.h> //Downloaded library for I2C display
+#include <SPI.h> //Library for SD card
+#include <SD.h> //Library for SD card
+
 File data;
  
+#define SD_CARD_PIN 10 //Change for the PIN that the CS output from SD card module is connected to
+
 LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE); // Initialize LCD Display at address 0x27 
+
 void setup()
 {
+  //Initialize serial port and lcd 
   Serial.begin(9600);
   lcd.begin (16,2);
+
   Serial.print("SD Card init...");
   //test if wiring is correct
-  if (!SD.begin(10)) {
+  if (!SD.begin(SD_CARD_PIN)) {
     Serial.println("init failed..");
     while (1);
   }
@@ -24,43 +27,54 @@ void setup()
  
 void loop()
 {
-  String brightLevel, secondLevel;
-  int value = analogRead(A0);
+  String firstLevel, secondLevel; //giving a "class" of brightness for LCD output
+
+  int first = analogRead(A0);
   int second = analogRead(A1);
-  if(value<100){
-    brightLevel = "Low";
+
+  //100 is a arbitrary value used for testing the output and photoresistor
+  if(first<100){
+    firstLevel = "Low";
   }else{
-    brightLevel = "High";
+    firstLevel = "High";
   }
   if(second<100){
     secondLevel = "Low";
   }else{
     secondLevel = "High";
   }
+
   lcd.setBacklight(HIGH); //Set Back light turn On
+  
   lcd.setCursor(0,0); // Move cursor to 0
-  lcd.print(value); 
+    lcd.print(first); 
   lcd.setCursor(5,0);
-  lcd.print(second);
-  lcd.setCursor(0,2);
-  lcd.print(brightLevel);
-  lcd.setCursor(5,2);
-  lcd.print(secondLevel);
+    lcd.print(second);
+  lcd.setCursor(0,1);
+    lcd.print(firstLevel);
+  lcd.setCursor(5,1);
+    lcd.print(secondLevel);
 
   
   data = SD.open("data.txt", FILE_WRITE); //open file
   if (data) {
-    data.print(value); //print the data to file
-    data.print(",");
-    data.print(second);
-    data.println();
+    data.print(first);  //print the data to file
+    data.print(",");    //Manually creating csv logic - to be fixed later 
+    data.print(second); //second sensor reading
+    data.println();     //create new line for data
 
     data.close();
-    Serial.println(value);
+
+    //just outputting one value to the serial monitor to ensure data is sending correctly
+    Serial.println(first);
+  
   } else {
+    //if you cannot open the data file it will give an error in serial monitor
     Serial.println("Cannot open file");
   }
+
   delay(1000);
-  lcd.clear();
+  
+  lcd.clear(); //Clearing display
   
 }
